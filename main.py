a27fb07fcf87e1d20fa1ca481324e1638c38b913
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-05-24 22:33:45 +0530

    fastapi server user and server response

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
+def root():
+    return {"message":"Server is running! Visit/docs for API documentation."}
+
+@app.get("/home")
 def home():
     return {"message": "FastAPI working!"}
+@app.get("/msg")
+def msg():
+    return {"message": "Home Page!"}
+@app.get("/user")
+def user():
+    return {"name":"S.Mohammed Nijamuddin","role":"AI backend engineer"}
