bc3a9fc84f43547f3a2f286c5b1cce8ba1a8253a
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-02 02:38:09 +0530

    Added PUT

--- Product_API.py
+++ Product_API.py
@@ -22,3 +22,12 @@ def create_product(prdct:Product):
 @app.get("/product")
 def get_product():
     return product
+
+#PUT
+
+@app.put("/product/{product_id}")
+def update_product(product_id:int,prdct:Product):
+    if product_id<0 or product_id>=len(product):
+        raise HTTPException(status_code=404,details="Product notfound")
+    product[product_id]=prdct
+    return {"message":"Product updated"}
