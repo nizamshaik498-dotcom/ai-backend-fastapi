80ad2a0d24023f949a68acef169a0d388e0ce603
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-02 00:24:08 +0530

    Updated PUT

--- Book_API.py
+++ Book_API.py
@@ -25,7 +25,7 @@ def get_books():
 #PUT
 @app.put("/books/{book_id}")
 def update_book(book_id:int,book:Book):
-    if id<0 or id>=len(books):
+    if book_id<0 or book_id>=len(books):
         raise HTTPException(status_code=404,detail="Book not found")
-    books[id]=book
+    books[book_id]=book
     return{"message":"Book Updated","data":book}
