59f17b76f69a62062f3991a391c6760a87800e1c
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-03 16:37:38 +0530

    Update Product_API.py

--- Product_API.py
+++ Product_API.py
@@ -11,23 +11,21 @@ class Product(BaseModel):
     brand:str
 
 #POST
-
 @app.post("/product")
 def create_product(prdct:Product):
     product.append(prdct)
     return{"message":"product added","data":prdct}
 
 #Get
-
 @app.get("/product")
 def get_product():
     return product
 
 #PUT
-
 @app.put("/product/{product_id}")
 def update_product(product_id:int,prdct:Product):
     if product_id<0 or product_id>=len(product):
         raise HTTPException(status_code=404,details="Product notfound")
     product[product_id]=prdct
     return {"message":"Product updated"}
+
