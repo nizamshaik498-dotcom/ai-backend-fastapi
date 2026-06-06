08c6b4e27c4beb22f544b09a12279081b98ef748
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-06 21:00:12 +0530

    p

---
+++ insert_book.py
@@ -0,0 +1,17 @@
+import sys
+import psycopg2
+print(sys.excecutable)
+
+conn=psycopg2.connect(
+    host="localhost",
+    database="fastapi_db",
+    user="postgres",
+    password="nizam2815"
+
+)
+
+cur=conn.cursor()
+
+cur.execute("INSERT INTO books(id,title,author,price)VALUES(1,'Fastapi_Basics','Alex',500)")
+conn.commit()
+print("Boook inserted successfully")
