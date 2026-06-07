6a41ceb15751aa4c7bde6206c01173edae22353d
S.Mohammed Nijamuddin <nizamshaik498@gmail.com>
2026-06-07 21:55:35 +0530

    Update Car_API.py

--- Car_API.py
+++ Car_API.py
@@ -19,3 +19,11 @@ def create_car(cars:Car):
 app.get("/car")
 def get_car():
     return car
+
+#PUT
+@app.put("/car/{car_id}")
+def update_car(car_id:int,cars:Car):
+    if car_id<0 or car_id>=len(car):
+        raise HTTPException(status_code=404,details="Car not found")
+    car[car_id]=cars
+    return {"message":"Car updated"}
